4e45102f5be8804bf3485e1674cc57b01fa21290
sara2hussein <sara2.7ussein@gmail.com>
2025-04-14 19:52:18 +0200

    Delete lib directory

--- lib/UI/tabs/hadeth/hadeth_model.dart
+++
@@ -1,5 +0,0 @@
-class Hadeth{
-  String title, content;
-Hadeth({required this.title, required this.content}); 
-
-}
--- lib/utils/shared_prefrances.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class PrefKeys {
-  static const String mostRecentKey = 'most_recent_key';
-}
-
-
-
